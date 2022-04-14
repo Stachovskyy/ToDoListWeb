cd693c7eb3f910d2ec7676982c62f86cb537614b
Bartek Swiderski <brt.swiderski@gmail.com>
2022-04-14 16:08:18 +0200

    Created first data models

--- ToDoListWeb/Data/Task.cs
+++ ToDoListWeb/Data/Task.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListWeb.Data
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public DateTime FinishDateTime { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public Size Size { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+
+    public class Size
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Task> Tasks { get; set; }
+    }
+
+    public class TaskBoard
+    {
+        public List<Task> List { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
